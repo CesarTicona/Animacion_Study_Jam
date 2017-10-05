3e0c1b129f4218ec82146bcf0198f4cb23e8a89b
Cesar <cesarticona.cc92@gmail.com>
2017-10-05 11:22:44 -0400

    cambios en el gradel 2

--- app/src/main/java/com/example/cesar/animacion1_study_jam/Splashscreen.java
+++ app/src/main/java/com/example/cesar/animacion1_study_jam/Splashscreen.java
@@ -9,37 +9,14 @@ import android.widget.ImageView;
 
 public class Splashscreen extends AppCompatActivity {
 
-    private ImageView logo;
-    private Animation animacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splashscreen);
-        getSupportActionBar().hide();
 
-        logo = (ImageView)findViewById(R.id.idlogo);
-        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splashscreen);
-        logo.startAnimation(animacion);
 
-        animacion.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
     }
 
 }
