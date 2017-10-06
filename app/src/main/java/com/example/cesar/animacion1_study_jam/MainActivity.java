3817f5cbd8abf52937df7cddf48c423785c4aed8
Cesar <cesarticona.cc92@gmail.com>
2017-10-05 23:44:41 -0400

    proyecto terminado

--- app/src/main/java/com/example/cesar/animacion1_study_jam/MainActivity.java
+++ app/src/main/java/com/example/cesar/animacion1_study_jam/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,40 +24,67 @@ public class MainActivity extends AppCompatActivity {
         imagen2 = (ImageView) findViewById(R.id.idImagen2);
     }
 
+
     public void zoomin(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        imagen1.startAnimation(animacion);
     }
 
     public void zoomout(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+        imagen1.startAnimation(animacion);
     }
 
-    public void fadein() {
+    public void fadein(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        imagen1.startAnimation(animacion);
     }
 
-    public void fadeout() {
+    public void fadeout(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+        imagen1.startAnimation(animacion);
     }
 
-    public void rotate() {
+    public void rotate(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
+        imagen1.startAnimation(animacion);
     }
 
-    public void move() {
+    public void move(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.move);
+        imagen1.startAnimation(animacion);
     }
 
-    public void slideup() {
+    public void slideup(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up);
+        imagen1.startAnimation(animacion);
     }
 
-    public void slidedown() {
+    public void slidedown(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
+        imagen1.startAnimation(animacion);
     }
 
-    public void blink() {
+    public void blink(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
+        imagen1.startAnimation(animacion);
+    }
+    public void bounce(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce);
+        imagen1.startAnimation(animacion);
     }
 
-    public void bounce() {
+    public void crossfade(View v) {
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        imagen1.startAnimation(animacion);
+        Animation animacion2 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+        imagen1.startAnimation(animacion2);
+        imagen2.startAnimation(animacion);
     }
 
-    public void crossfade() {
-    }
-
-    public void sequential() {
+    public void stop(View v) {
+        imagen1.clearAnimation();
+        imagen2.clearAnimation();
     }
 
 }
